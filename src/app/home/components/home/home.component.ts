074f87981efe79dd35df77699a6d6f4d3d21bc23
DavidBoccalandro <davidboccalandro@hotmail.com>
2022-06-22 13:24:31 -0300

    adding info to menu from state of user logged

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { LoginResponse } from 'src/app/auth/interfaces/login-response.interface';
 import { LoginService } from 'src/app/auth/services/login.service';
+import { AppStateWithUser } from 'src/app/auth/store/reducer/user.reducer';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +12,23 @@ import { LoginService } from 'src/app/auth/services/login.service';
 })
 export class HomeComponent implements OnInit {
   navigation: string = '';
+  menuInfo!: LoginResponse;
+  userSubscription!: Subscription;
 
-  constructor(private loginService: LoginService) {}
+  constructor(
+    private loginService: LoginService,
+    private store: Store<AppStateWithUser>
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.userSubscription = this.store.select('user').subscribe((data) => {
+      this.menuInfo = data;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
+  }
 
   navigationHandler(title: string) {
     this.navigation = title;
